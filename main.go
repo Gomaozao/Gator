90d15cdb50f40bda7e7d1b7bf7c7baa92722a7a6
Gomaozao <40327219+Gomaozao@users.noreply.github.com>
2026-06-17 17:50:22 -0300

    add handler_user and create user functionality

--- main.go
+++ main.go
@@ -1,68 +1,53 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	"database/sql"
 	"log"
 	"os"
 
-	"Gator/internal/config"
+	"github.com/bootdotdev/gator/internal/config"
+	"github.com/bootdotdev/gator/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
-	config_ptr *config.Config
-}
-
-type command struct {
-	name string
-	args []string
-}
-
-type commands struct {
-	commandName map[string]func(*state, command) error
-}
-
-func (c *commands) run(s *state, cmd command) error {
-	cmdName, ok := c.commandName[cmd.name]
-	if ok {
-		return cmdName(s, cmd)
-	}
-	return errors.New("Command not found.")
-}
-
-func (c *commands) register(name string, f func(*state, command) error) {
-	c.commandName[name] = f
-}
-
-func handlerLogin(s *state, cmd command) error {
-	if len(cmd.args) == 0 {
-		return errors.New("No Username provided.")
-	}
-	if err := s.config_ptr.SetUser(cmd.args[0]); err != nil {
-		return errors.New("Something went wrong with the name.")
-	}
-	fmt.Println("Name has been set.")
-	return nil
+	db  *database.Queries
+	cfg *config.Config
 }
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("failed to read config: %v", err)
+		log.Fatalf("error reading config: %v", err)
 	}
 
-	foo := state{&cfg}
-	bar := commands{commandName: make(map[string]func(*state, command) error)}
-	bar.register("login", handlerLogin)
-	if len(os.Args) < 2 {
-		fmt.Println("error")
-		os.Exit(1)
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
 	}
+	defer db.Close()
+	dbQueries := database.New(db)
+
+	programState := &state{
+		db:  dbQueries,
+		cfg: &cfg,
+	}
+
+	cmds := commands{
+		registeredCommands: make(map[string]func(*state, command) error),
+	}
+	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
+	if len(os.Args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+	}
+
 	cmdName := os.Args[1]
-	args := os.Args[2:]
-	usrcmd := command{cmdName, args}
-	if err := bar.run(&foo, usrcmd); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	cmdArgs := os.Args[2:]
+
+	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	if err != nil {
+		log.Fatal(err)
 	}
 }
